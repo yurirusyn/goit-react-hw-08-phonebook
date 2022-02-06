2bb1702182827ec0ddf405914457315da4ede3ab
yurirusyn <iirusyn@gmail.com>
2022-02-06 13:37:03 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -55,6 +55,23 @@ class App extends Component {
       contacts: prev.contacts.filter(el => el.id !== id),
     }));
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
   render() {
     const { addContacts, filtercontacts, removeContacts } = this;
     return (
